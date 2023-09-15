fab9b4a222b6e069df73877e7e98e7aef8bc48c4
Joseph Tzeng <joseph@pushcorn.com>
2023-09-15 14:55:42 -0400

    adds fields option to HashCode

--- public/lib/plugins/HashCode.js
+++ public/lib/plugins/HashCode.js
@@ -2,12 +2,20 @@ module.exports = function (nit, Self)
 {
     return (Self = nit.definePlugin ("HashCode"))
         .use ("nit.utils.String")
+        .field ("[fields...]", "string", "The fields to be hashed.")
         .staticMethod ("onUsePlugin", function (hostClass, plugin)
         {
             hostClass
                 .getter ("hashCode", function ()
                 {
-                    return Self.String.intHash (nit.toJson ([this.constructor.name, this.toPojo ()]));
+                    var pojo = this.toPojo ();
+
+                    if (plugin.fields.length)
+                    {
+                        pojo = nit.pick (pojo, plugin.fields);
+                    }
+
+                    return Self.String.intHash (nit.toJson ([this.constructor.name, pojo]));
                 })
             ;
         })
--- test/tests/public/lib/plugins/HashCode.js
+++ test/tests/public/lib/plugins/HashCode.js
@@ -1,10 +1,19 @@
+const Person = nit.defineClass ("Person")
+    .field ("<name>", "string")
+    .field ("<age>", "integer")
+;
+
+
 test.method ("plugins.HashCode", "onUsePlugin", true)
     .should ("add the hashCode getter to the host class")
-    .given (nit.defineClass ("Person")
-        .field ("<name>", "string")
-        .field ("<age>", "integer")
-    )
-    .after (s => s.person = new s.args[0] ("John Doe", 55))
-    .expectingPropertyToBe ("person.hashCode", 1622029782)
-    .commit ()
+        .given (Person, nit.new ("plugins.HashCode"))
+        .after (s => s.person = new Person ("John Doe", 55))
+        .expectingPropertyToBe ("person.hashCode", 1622029782)
+        .commit ()
+
+    .can ("hash only the specifed fields")
+        .given (Person, nit.new ("plugins.HashCode", "age"))
+        .after (s => s.person = new Person ("John Doe", 56))
+        .expectingPropertyToBe ("person.hashCode", 1703114347)
+        .commit ()
 ;
