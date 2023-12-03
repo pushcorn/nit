4fb9939e0aee352c1814828110debbf072862820
Joseph Tzeng <joseph@pushcorn.com>
2023-12-03 13:09:58 -0500

    adds returnIf... stages to the Server queue

--- public/lib/plugins/Server.js
+++ public/lib/plugins/Server.js
@@ -9,7 +9,10 @@ module.exports = function (nit, Self)
         .onUsedBy (function (hostClass)
         {
             hostClass
-                .plugin ("lifecycle-component", "start", "stop")
+                .do (!hostClass.lookupPlugin ("lifecycle-component"), function ()
+                {
+                    hostClass.plugin ("lifecycle-component", "start", "stop");
+                })
                 .property ("state", "string", "stopped",
                 {
                     writer: writer,
@@ -19,81 +22,89 @@ module.exports = function (nit, Self)
                 .property ("stopResult", "Promise", { writer: writer, enumerable: false })
                 .onConfigureQueueForStart (function (queue, self, args)
                 {
-                    queue.replace ("start.invokeHook", function ()
-                    {
-                        if (self.state == "stopping")
+                    queue
+                        .after ("preStart", "preStart.returnIfStarted", function ()
                         {
-                            Self.throw ("error.invalid_start_state");
-                        }
-
-                        if (self.state == "starting" || self.state == "started")
-                        {
-                            return self.startResult;
-                        }
-
-                        var def = new nit.Deferred;
-                        var cls = self.constructor;
-
-                        self.state = writer.value ("starting");
-                        self.startResult = writer.value (def.promise);
-
-                        nit.invoke.then ([self, cls[cls.kStart]], args, function (e)
-                        {
-                            if (e)
+                            if (self.state == "stopping")
                             {
-                                self.state = writer.value ("stopped");
-
-                                def.reject (e);
+                                Self.throw ("error.invalid_start_state");
                             }
-                            else
+
+                            if (self.state == "starting" || self.state == "started")
                             {
-                                self.state = writer.value ("started");
-
-                                def.resolve (self);
+                                return nit.Queue.Stop (self.startResult);
                             }
-                        });
+                        })
+                        .replace ("start.invokeHook", function ()
+                        {
+                            var def = new nit.Deferred;
+                            var cls = self.constructor;
 
-                        return def.promise;
-                    });
+                            self.state = writer.value ("starting");
+                            self.startResult = writer.value (def.promise);
+
+                            nit.invoke.then ([self, cls[cls.kStart]], args, function (e)
+                            {
+                                if (e)
+                                {
+                                    self.state = writer.value ("stopped");
+
+                                    def.reject (e);
+                                }
+                                else
+                                {
+                                    self.state = writer.value ("started");
+
+                                    def.resolve (self);
+                                }
+                            });
+
+                            return def.promise;
+                        })
+                    ;
                 })
                 .onConfigureQueueForStop (function (queue, self, args)
                 {
-                    queue.replace ("stop.invokeHook", function ()
-                    {
-                        if (self.state == "starting")
+                    queue
+                        .after ("preStop", "preStop.returnIfStopped", function ()
                         {
-                            Self.throw ("error.invalid_stop_state");
-                        }
-
-                        if (self.state == "stopping" || self.state == "stopped")
-                        {
-                            return self.stopResult;
-                        }
-
-                        var def = new nit.Deferred;
-                        var cls = self.constructor;
-
-                        self.state = writer.value ("stopping");
-                        self.stopResult = writer.value (def.promise);
-
-                        nit.invoke.then ([self, cls[cls.kStop]], args, function (e)
-                        {
-                            if (e)
+                            if (self.state == "starting")
                             {
-                                self.state = writer.value ("started");
-
-                                def.reject (e);
+                                Self.throw ("error.invalid_stop_state");
                             }
-                            else
+
+                            if (self.state == "stopping" || self.state == "stopped")
                             {
-                                self.state = writer.value ("stopped");
-
-                                def.resolve (self);
+                                return nit.Queue.Stop (self.stopResult);
                             }
-                        });
+                        })
+                        .replace ("stop.invokeHook", function ()
+                        {
+                            var def = new nit.Deferred;
+                            var cls = self.constructor;
 
-                        return def.promise;
-                    });
+                            self.state = writer.value ("stopping");
+                            self.stopResult = writer.value (def.promise);
+
+                            nit.invoke.then ([self, cls[cls.kStop]], args, function (e)
+                            {
+                                if (e)
+                                {
+                                    self.state = writer.value ("started");
+
+                                    def.reject (e);
+                                }
+                                else
+                                {
+                                    self.state = writer.value ("stopped");
+
+                                    def.resolve (self);
+                                }
+                            });
+
+                            return def.promise;
+                        })
+                    ;
                 })
             ;
         })
